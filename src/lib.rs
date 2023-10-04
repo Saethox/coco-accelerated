beef8cfeef309e0c16ecb439ad81ad24d91dbb4c
Jonathan Wurth <jonathan.wurth@uni-a.de>
2023-10-04 20:39:49 +0200

    Add missing re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
 #![allow(non_snake_case)]
 
+mod batch;
 pub mod bbob;
 
-mod batch;
-pub use crate::batch::InputBatch;
+// Re-exports.
+pub use coco_futhark::Context;
 
-/// Re-export.
+pub mod backends {
+    pub use coco_futhark::backends::Backend;
+    #[cfg(feature = "cuda")]
+    pub use coco_futhark::backends::Cuda;
+    #[cfg(feature = "multicore")]
+    pub use coco_futhark::backends::MultiCore;
+    #[cfg(feature = "opencl")]
+    pub use coco_futhark::backends::OpenCl;
+    #[cfg(feature = "c")]
+    pub use coco_futhark::backends::C;
+}
+
 pub use strum::IntoEnumIterator;
+
+pub use crate::batch::InputBatch;
